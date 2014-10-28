e835ab73b2dc66380ca4aaa2a5b5bdfa71e9caaa
RomanLichev <roman.l.l@abv.bg>
2014-10-28 13:12:20 +0200

    new version: 1.Is included information for section News, Events, Donation, Contacts. 2.Changes in menu nav (nav have border and hover background-coolr change). 3.Removed image in the header of the page. 4.Fixed spelling.

---
+++ NewVision/script/content-injector.js
@@ -0,0 +1,25 @@
+﻿function httpGet(documentName) {
+    var xmlHttp = null;
+
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open('GET', /*'content/' + */documentName + '.html', false);
+    xmlHttp.send(null);
+    document.getElementById('wrapper').innerHTML = xmlHttp.responseText;
+    //return xmlHttp.responseText;
+}
+
+function getPageName() {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    if (!vars[0]) {
+        return 'main';
+    }
+
+    var page = vars[vars.length - 1].split('page=')[1];
+    return /*vars[vars.length - 1]*/ page || 'error';
+}
+
+
+var pageName = getPageName();
+
+httpGet(pageName);
